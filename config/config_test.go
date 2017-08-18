4ab801282ce481fdca33bd84398a0b97c0fdae13
Bill Hazard <bhazard2@gmail.com>
2017-08-18 17:35:08 -0400

    Initial config tests

--- config/config_test.go
+++ config/config_test.go
@@ -8,3 +8,19 @@ import (
 
   "github.com/stretchr/testify/assert"
 )
+
+func TestReadConfig(t *testing.T) {
+  // Tests with different params from config file
+  absPath, err := filepath.Abs("../tests/")
+
+  assert.NotNil(t, absPath)
+  assert.Nil(t, err)
+
+  config := &Config{}
+
+  // Reads second config file
+  err = cfgfile.Read(config, absPath+"/config2.yml")
+  assert.Nil(t, err)
+
+  assert.Equal(t, uint64(0), config.SpoolSize)
+}
